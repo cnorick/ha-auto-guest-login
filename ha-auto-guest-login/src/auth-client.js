d47b656b219ec4c1b373f5d804b3bb5a31896c1b
Nathan Orick <cnathanorick@gmail.com>
2023-10-26 13:08:01 -0400

    feat(): add advanced config options (#24)
    also add more logging

--- ha-auto-guest-login/src/auth-client.js
+++ ha-auto-guest-login/src/auth-client.js
@@ -6,23 +6,34 @@ export class AuthClient {
     this.password = password;
   }
 
-  async getRedirectUri(haUrl, dashboard) {
-    var code = await this.#getCode(haUrl);
-    var uri = this.#createRedirectUri(haUrl, code, dashboard);
+  async getRedirectUri(internalUrl, redirectBaseUrl, dashboard) {
+    const clientId = this.#getClientId(redirectBaseUrl);
+    console.log(`using clientId: ${clientId}`);
+
+    const code = await this.#getCode(internalUrl, redirectBaseUrl, clientId);
+    console.log(`code: ${code}`);
+
+    const uri = this.#createRedirectUri(redirectBaseUrl, code, clientId, dashboard);
     return uri;
   }
 
-  async #getCode(haUrl) {
-    const flowId = await this.#getFlowId(haUrl);
+  async #getCode(internalUrl, redirectBaseUrl, clientId) {
+    const flowId = await this.#getFlowId(internalUrl, redirectBaseUrl, clientId);
+    console.log(`flowId: ${flowId}`);
+
+    const url = `${internalUrl}/auth/login_flow/${flowId}`;
+    console.log('fetching code');
+    console.log(`\turl: ${url}`);
+
     const response = await fetch(
-      `${haUrl}/auth/login_flow/${flowId}`,
+      url,
       {
         headers: {
           accept: "*/*",
           "accept-language": "en",
           "content-type": "text/plain;charset=UTF-8",
         },
-        body: `{"username":"${this.username}","password":"${this.password}","client_id":"${this.#getClientId(haUrl)}"}`,
+        body: `{"username":"${this.username}","password":"${this.password}","client_id":"${clientId}"}`,
         method: "POST",
       }
     );
@@ -45,14 +56,20 @@ export class AuthClient {
     return baseUrl + '/';
   }
 
-  async #getFlowId(baseUrl) {
-    const response = await fetch(`${baseUrl}/auth/login_flow`, {
+  async #getFlowId(baseAuthUrl, baseRedirectUrl, clientId) {
+    const url = `${baseAuthUrl}/auth/login_flow`;
+    const redirectUri = `${baseRedirectUrl}?auth_callback=1`;
+    console.log(`fetching flowId`);
+    console.log(`\turl: ${url}`);
+    console.log(`\tredirectUri: ${redirectUri}`);
+
+    const response = await fetch(url, {
       headers: {
         accept: "*/*",
         "accept-language": "en",
         "content-type": "text/plain;charset=UTF-8",
       },
-      body: `{"client_id":"${this.#getClientId(baseUrl)}","handler":["homeassistant",null],"redirect_uri":"${baseUrl}?auth_callback=1"}`,
+      body: `{"client_id":"${clientId}","handler":["homeassistant",null],"redirect_uri":"${redirectUri}"}`,
       method: "POST",
     });
 
@@ -63,20 +80,20 @@ export class AuthClient {
     return (await response.json()).flow_id;
   }
 
-  #getState(haUrl) {
+  #getState(haUrl, clientId) {
     const state = {
       hassUrl: `${haUrl}`,
-      clientId: this.#getClientId(haUrl),
+      clientId,
     };
     const jsonState = JSON.stringify(state);
     return Buffer.from(jsonState).toString('base64');
   }
 
-  #createRedirectUri(haUrl, code, dashboard) {
+  #createRedirectUri(haUrl, code, clientId, dashboard) {
     let uri = `${haUrl}/${dashboard}?auth_callback=1`;
     uri.includes("?") ? uri.endsWith("&") || (uri += "&") : (uri += "?");
     uri += `code=${encodeURIComponent(code)}`;
-    uri += `&state=${encodeURIComponent(this.#getState(haUrl))}`;
+    uri += `&state=${encodeURIComponent(this.#getState(haUrl, clientId))}`;
     uri += "&storeToken=true";
     return uri;
   }
